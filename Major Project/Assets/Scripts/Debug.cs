c70d7f8ac5b9ec53b0eb489555651b6ab943ab86
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2023-03-16 14:28:14 +0000

    Changed Debug script to dynamically find all the Collectible Item Data

--- Major Project/Assets/Scripts/Debug.cs
+++ Major Project/Assets/Scripts/Debug.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace Caleb
 {
     public class Debug : MonoBehaviour
     {
-        [SerializeField] private ItemData[] collectibleItems;
+        private List<ItemData> collectibleItems = new List<ItemData>();
+
+        void Start()
+        {
+            string[] assetNames = AssetDatabase.FindAssets("t:ItemData", new[] { "Assets/Prefabs/Item Data/Collectibles" });
+            collectibleItems.Clear();
+            foreach (string SOName in assetNames)
+            {
+                var SOpath = AssetDatabase.GUIDToAssetPath(SOName);
+                var itemData = AssetDatabase.LoadAssetAtPath<ItemData>(SOpath);
+                collectibleItems.Add(itemData);
+            }
+
+            foreach (ItemData item in collectibleItems)
+            {
+                print(item.name);
+            }
+        }
 
         private void OnApplicationQuit()
         {
